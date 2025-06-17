557bc7537304ccdf1dcb22e0ffd057316c0c8f25
Chiravuri, Indrani <Indrani.Chiravuri@ncrvoyix.com>
2025-06-17 11:26:46 +0530

    Add project files.

---
+++ NZWalksAPI/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalksAPI.Controllers
+{
+    //https://localhost:port/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private static readonly string[] StudentsNames = { "John", "Paul", "Indrani", "Chiravuri"};
+
+        //GET:https://localhost:port/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            return Ok(StudentsNames);
+        }
+    }
+}
